7d008d669c523023af582bda7e2b9a21f0aaa59f
samtha83 <69811190+samtha83@users.noreply.github.com>
2022-10-24 08:40:38 -0400

    Update streamlit_app.py
    formatted the json

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,10 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+fruityvice_normailized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normailized)
+
+
 
 
                
