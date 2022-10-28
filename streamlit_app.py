e3457d59775fded3b63a877239bd00c3ddcaf901
samtha83 <69811190+samtha83@users.noreply.github.com>
2022-10-28 07:44:36 -0400

    Update streamlit_app.py
    moved fruit load list to a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,19 +42,25 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT *  from FRUIT_LOAD_LIST")
+    return my_cur.fetchall()
+#my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT *  from FRUIT_LOAD_LIST")
+#my_cur.execute("SELECT *  from FRUIT_LOAD_LIST")
 #my_data_row = my_cur.fetchone()
-my_data_row = my_cur.fetchall()
-streamlit.text("The fruit load list contains:")
+#my_data_row = my_cur.fetchall()
+streamlit.button("The fruit load list contains:")
+my_data_row=get_fruit_load_list()
 streamlit.dataframe(my_data_row)
 
+streamlit.stop()       
 fruit_toadd = streamlit.text_input('What fruit would you like to add to the above list?')
 streamlit.write('Thanks for adding ', fruit_toadd)
 
 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
 
-               
+   
