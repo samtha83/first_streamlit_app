cad986e54b4570452293f43d975b3d0b905e45db
samtha83 <69811190+samtha83@users.noreply.github.com>
2022-10-28 08:09:22 -0400

    Update streamlit_app.py
    Added title

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,9 @@ except URLError as e:
   streamlit.error()
 
 
+streamlit.header('View Our Fruit List - Add Your Facorites!') 
+  
+  
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT *  from FRUIT_LOAD_LIST")
